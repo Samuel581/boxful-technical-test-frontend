31f1bb3d036a1fd1d36dbc5a4165fa794e502768
Samuel Ortiz <samuel.adonay2001@gmail.com>
2026-02-09 03:33:03 -0600

    🔧 Refactor authService to use constants for API routes
    - Updated register and login methods to utilize LOGIN_API and REGISTER_API constants for improved maintainability and consistency.

--- app/services/authService.ts
+++ app/services/authService.ts
@@ -1,17 +1,17 @@
 'use client'
+import { LOGIN_API, REGISTER_API } from "../constants/backendRoutes";
 import { api } from "../lib/api/axios";
 import { LoginDto, RegisterDto } from "../types/auth";
 
 export const authService = {
     // TODO, separate types and the other one I cannot remenber
     async register(data: RegisterDto){
-        const response = await api.post('/auth/register', data)
+        const response = await api.post(REGISTER_API, data);
         return response.data;
     },
 
     async login(data: LoginDto){
-        const response = await api.post('/auth/login', data)
-        console.log(response.data)
+        const response = await api.post(LOGIN_API, data);
         return response.data;
     }
 }
